97c087f2625363f6c45e4bc3bb3b524e135ec854
vivianeflowt <vivianeflowt@gmail.com>
2020-10-08 04:36:29 -0300

    some structure refactoring

--- src/fargs.js
+++ src/fargs.js
@@ -6,12 +6,41 @@
 
 'use strict'
 
-const core = require('./core')
 const type = require('type-detect')
-const LIST_TYPE = require('./defs').LIST_TYPE
+
+const LIST_TYPE = {
+  SIMPLE: (argList = []) => {
+    const list = []
+    argList.forEach(e => {
+      list.push([e])
+    })
+    return list
+  },
+  TYPED: (argList = []) => {
+    const list = []
+    argList.forEach(e => {
+      list.push([e, type(e).toLowerCase()])
+    })
+    return list
+  }
+}
+const argsList = (argList = [], callback) => {
+  if (callback == null) {
+    callback = LIST_TYPE.SIMPLE
+  }
+  return callback(argList)
+}
+
+const argsCount = (argList = []) => {
+  return argList.length
+}
+
+const typeOf = (value) => {
+  return type(value).toLowerCase()
+}
 
 const FArgs = function (argumentObject) {
-  if (!type(argumentObject) === 'Arguments') {
+  if (!typeOf(argumentObject) === 'arguments') {
     throw new Error('needs argument object as parameter')
   }
   const argObj = argumentObject
@@ -26,16 +55,21 @@ const FArgs = function (argumentObject) {
 
   return {
     count: () => {
-      return core.argsCount(args())
+      return argsCount(args())
     },
     list: () => {
-      return core.argsList(args(), LIST_TYPE.SIMPLE)
+      return argsList(args(), LIST_TYPE.SIMPLE)
     },
     typedList: () => {
-      return core.argsList(args(), LIST_TYPE.TYPED)
-    },
-    validate: (rules) => core.argsValidate(args(), rules)
+      return argsList(args(), LIST_TYPE.TYPED)
+    }
   }
 }
 
-module.exports.FArgs = FArgs
+module.exports = {
+  FArgs,
+  typeOf,
+  argsList,
+  argsCount,
+  LIST_TYPE
+}
