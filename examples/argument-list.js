bb277d3a710bc84c48ff9538b9153ae8c8949102
vivianeflowt <vivianeflowt@gmail.com>
2020-10-08 00:47:19 -0300

    example changes

--- examples/argument-list.js
+++ examples/argument-list.js
@@ -5,12 +5,10 @@ var sampleFunction = function () {
   // creates a new instance of the function parser and passes the
   // function's argument object as a parameter
   var fargs = FargsParser(arguments)
-  // to get the list of arguments we can use two methods
-  //  methods:
+
   //    list        (simple argument list)
   //    typedList   (list with argument type)
-  // each method returns an array with the data of the
-  // arguments passed to the function
+
   console.table(fargs.list)
   console.table(fargs.typedList)
 }
