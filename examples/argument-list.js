9ced22b599ed191afd15b7e3867513e803a7661f
vivianeflowt <vivianeflowt@gmail.com>
2020-10-09 03:32:24 -0300

    remove esdoc

--- examples/argument-list.js
+++ examples/argument-list.js
@@ -2,16 +2,15 @@ var FargsParser = require('../src/fargs-parser').FArgs
 
 // some function without argument definition
 var sampleFunction = function () {
+    // creates a new instance of the function parser and passes the
+    // function's argument object as a parameter
+    var fargs = FargsParser(arguments)
 
-  // creates a new instance of the function parser and passes the
-  // function's argument object as a parameter
-  var fargs = FargsParser(arguments)
+    // list        (simple argument list)
+    // typedList   (list with argument type)
 
-  // list        (simple argument list)
-  // typedList   (list with argument type)
-
-  console.table(fargs.list())
-  console.table(fargs.typedList())
+    console.table(fargs.list())
+    console.table(fargs.typedList())
 }
 
 sampleFunction('name', 123)
--- src/fargs-parser.js
+++ src/fargs-parser.js
@@ -8,94 +8,93 @@
 
 const type = require('type-detect')
 
-
 const LIST_TYPE = {
-  SIMPLE: (argList = []) => {
-    /**
-     * @param {array} argList
-     * @return {array} result a array with simple list
-     */
-    const list = []
-    argList.forEach((e) => {
-      list.push([e])
-    })
-    return list
-  },
-  TYPED: (argList = []) => {
-    /**
-     * @param {array} argList
-     * @return {array} result a array with a typed
-     */
-    const list = []
-    argList.forEach((e) => {
-      list.push([e, typeOf(e)])
-    })
-    return list
-  },
+    SIMPLE: (argList = []) => {
+        /**
+         * @param {array} argList
+         * @return {array} result a array with simple list
+         */
+        const list = []
+        argList.forEach((e) => {
+            list.push([e])
+        })
+        return list
+    },
+    TYPED: (argList = []) => {
+        /**
+         * @param {array} argList
+         * @return {array} result a array with a typed
+         */
+        const list = []
+        argList.forEach((e) => {
+            list.push([e, typeOf(e)])
+        })
+        return list
+    },
 }
 const argsList = (argList = [], callback) => {
-  /**
-   * @param {array} argList
-   * @param {function} callback
-   * @return {array} result a array with the arguments list
-   */
-  if (callback == null) {
-    callback = LIST_TYPE.SIMPLE
-  }
-  return callback(argList)
+    /**
+     * @param {array} argList
+     * @param {function} callback
+     * @return {array} result a array with the arguments list
+     */
+    if (callback == null) {
+        callback = LIST_TYPE.SIMPLE
+    }
+    return callback(argList)
 }
 
 const argsCount = (argList = []) => {
-  /**
-   * @param {array} argList
-   * @return {number} result length of array
-   */
-  return argList.length
+    /**
+     * @param {array} argList
+     * @return {number} result length of array
+     */
+    return argList.length
 }
 
 const typeOf = (value) => {
-  /**
-   * @param {value} value some value to get the type
-   * @return {string} result value type
-   */
-  return type(value).toLowerCase()
+    /**
+     * @param {value} value some value to get the type
+     * @return {string} result value type
+     */
+    return type(value).toLowerCase()
 }
 
 const FArgs = function (argumentObject) {
-  /**
-   * @param {arguments} argumentObject the function argument object
-   * @return {object} result a fargs-parser instance
-   */
-  if (!typeOf(argumentObject) === 'arguments') {
-    throw new Error('needs argument object as parameter')
-  }
-  const argObj = argumentObject
-  let argList = null
-
-  const args = () => {
-    if (argList === null) {
-      argList = Array.from(argObj)
+    /**
+     * @param {arguments} argumentObject the function argument object
+     * @return {object} result a fargs-parser instance
+     */
+    if (!typeOf(argumentObject) === 'arguments') {
+        throw new Error('needs argument object as parameter')
     }
-    return argList
-  }
+    const argObj = argumentObject
+    let argList = null
 
-  return {
-    count: () => {
-      return argsCount(args())
-    },
-    list: () => {
-      return argsList(args(), LIST_TYPE.SIMPLE)
-    },
-    typedList: () => {
-      return argsList(args(), LIST_TYPE.TYPED)
-    },
-  }
+    const args = () => {
+        if (argList === null) {
+            argList = Array.from(argObj)
+        }
+        return argList
+    }
+
+    return {
+        count: () => {
+            return argsCount(args())
+        },
+        list: () => {
+            return argsList(args(), LIST_TYPE.SIMPLE)
+        },
+        typedList: () => {
+            return argsList(args(), LIST_TYPE.TYPED)
+        },
+    }
 }
 
 module.exports = {
-  FArgs,
-  typeOf,
-  argsList,
-  argsCount,
-  LIST_TYPE,
+    FArgs,
+    typeOf,
+    argsList,
+    argsCount,
+    LIST_TYPE,
 }
