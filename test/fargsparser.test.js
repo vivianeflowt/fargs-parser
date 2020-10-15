be5fc9bc23efb15daf7dbda809686b35c3358427
vivianeflowt <vivianeflowt@gmail.com>
2020-10-15 07:24:09 -0300

    tests update

--- test/fargsparser.test.js
+++ test/fargsparser.test.js
@@ -2,30 +2,29 @@ const assert = require('assert')
 const typeOf = require('../src/fargs-parser').typeOf
 const FArgsParser = require('../src/fargs-parser').FArgsParser
 
-
 test('arguments length', () => {
-  const testing = function () {
-    const fargs = FArgsParser(arguments)
-    return fargs.length
-  }
-  assert.strictEqual(testing('name', 1), 2)
-  assert.strictEqual(testing('name', 1, 2, 3), 4)
+    const testing = function () {
+        const fargs = FArgsParser(arguments)
+        return fargs.length
+    }
+    assert.strictEqual(testing('name', 1), 2)
+    assert.strictEqual(testing('name', 1, 2, 3), 4)
 })
 
 test('arguments type', () => {
-  const testing = function () {
-    const fargs = FArgsParser(arguments)
-    return fargs.typeOf(0)
-  }
-  assert.strictEqual(testing('name'), 'string')
-  assert.strictEqual(testing(1), 'number')
-  assert.strictEqual(testing({}), 'object')
-  assert.strictEqual(testing([]), 'array')
+    const testing = function () {
+        const fargs = FArgsParser(arguments)
+        return fargs.typeOf(0)
+    }
+    assert.strictEqual(testing('name'), 'string')
+    assert.strictEqual(testing(1), 'number')
+    assert.strictEqual(testing({}), 'object')
+    assert.strictEqual(testing([]), 'array')
 })
 
 test('needs arguments object', () => {
-  const testing = function () {
-    const fargs = FArgsParser()
-  }
-  expect(testing).toThrow(Error);
+    const testing = function () {
+        const fargs = FArgsParser()
+    }
+    expect(testing).toThrow(Error)
 })
